62d6d2509b30b64fd475b6e9ecbb04b3a202ee41
Jinawong <jokjack26172@gmail.com>
2020-09-16 15:05:46 +0700

    add upload for testing limit file from nginx

--- blog-service/index.js
+++ blog-service/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const Hapi = require("@hapi/hapi");
+const fs = require("fs");
+const Joi = require("@hapi/joi");
 
 const init = async () => {
   const server = Hapi.server({
@@ -13,6 +15,46 @@ const init = async () => {
     path: "/",
     handler: (request, h) => {
       return "Hello ! this is an Blog Service.";
+    },
+  });
+
+  server.route({
+    method: "POST",
+    path: "/upload",
+    options: {
+      payload: {
+        maxBytes: 1048576 * 100,
+        parse: true,
+        output: "stream",
+        allow: "multipart/form-data",
+        multipart: true,
+      },
+      validate: {
+        payload: Joi.object({
+          file: Joi.any().required(),
+        }),
+      },
+    },
+    handler: (request, h) => {
+      const data = request.payload;
+      if (data.file) {
+        const name = data.file.hapi.filename;
+        const path = __dirname + "/uploads/" + name;
+        const file = fs.createWriteStream(path);
+
+        file.on("error", (err) => console.error(err));
+
+        data.file.pipe(file);
+
+        data.file.on("end", (err) => {
+          const ret = {
+            filename: data.file.hapi.filename,
+            headers: data.file.hapi.headers,
+          };
+          return JSON.stringify(ret);
+        });
+      }
+      return { message: "upload success." };
     },
   });
 
